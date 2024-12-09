cf662955a5f126815eb22d831837e345e5558cab
shiburizu <shiburizu312@gmail.com>
2024-12-09 05:33:39 +0000

    tests

--- servbot.py
+++ servbot.py
@@ -17,7 +17,7 @@ from discord.ext import commands
 from discord.ext.tasks import loop
 from pyairtable import Api
 
-TwitterClient = twClient('en-US',user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0")
+TwitterClient = twClient('en-US',user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
 TwitterRegex = r"(?:x|twitter)\.com\/([^\/]+)\/status\/([^\/?\s]+)"
 
 lastSiteRebuild = 0
@@ -516,7 +516,6 @@ async def login_twitter():
 	else:
 		await TwitterClient.login(
 			auth_info_1=config['DEFAULT']['TwitterUser'],
-			auth_info_2=config['DEFAULT']['TwitterEmail'],
 			password=config['DEFAULT']['TwitterPass']
 		)
 		TwitterClient.save_cookies(config['DEFAULT']['CookiesFile'])
