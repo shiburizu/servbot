640da74f7814b4fbcbbaa271f718bb342dd65d68
shiburizu <shiburizu312@gmail.com>
2025-08-06 00:33:09 -0400

    updates

--- servbot-disnake.py
+++ servbot-disnake.py
@@ -212,7 +212,7 @@ async def task(ctx, task: str = commands.Param(description='Task name.',autocomp
 		if priority == None:
 			priority = 'Todo'
 		taskTbl = at.table(config['DEFAULT']['taskBase'],config['DEFAULT']['taskTable'])
-		new_task = taskTbl.create({'Task': task, 'Project': project_id, 'Description': comment, 'Assignees': whomst_ids, 'Due Date': due, 'Status': status, 'Task Priority': priority, 'Attachments': attachment_dict})
+		new_task = taskTbl.create({'Task': task, 'Project': project_id, 'Description': comment, 'Assignees': whomst_ids, 'Due Date': due, 'Status': status, 'Task Priority': priority, 'Attachments': attachment_dict},typecast=True)
 		await ctx.response.send_message("Created Task: [%s](%s)%s" % (new_task['fields']['Task'],new_task['fields']['Interface URL'],extra),suppress_embeds=True)
 		await refresh_slash_data()
 
@@ -261,7 +261,7 @@ async def project(ctx, project: str = commands.Param(description='Project name. 
 			priority = 'Todo'
 		# create new project
 		projTbl = at.table(config['DEFAULT']['projBase'],config['DEFAULT']['projTable'])
-		new_proj = projTbl.create({'Project': project,'Description': comment,'Event': event_id,'Status': status,'Project Priority': priority,'Due Date': due,'Attachments': attachment_dict})
+		new_proj = projTbl.create({'Project': project,'Description': comment,'Event': event_id,'Status': status,'Project Priority': priority,'Due Date': due,'Attachments': attachment_dict},typecast=True)
 		await ctx.response.send_message("Created Project: [%s](%s)" % (new_proj['fields']['Project'],new_proj['fields']['Interface URL']),suppress_embeds=True)
 		await refresh_slash_data()
 
@@ -300,7 +300,7 @@ async def timeline(ctx, name: str = commands.Param(description='Timeline event n
 		extra += "\nIgnored unknown assignees: %s" % ",".join(ignored)
 
 	taskTbl = at.table(config['DEFAULT']['timeBase'],config['DEFAULT']['timeTable'])
-	new_task = taskTbl.create({'Project': name,'Type': type,'Comments': comments,'Assignees': whomst_ids,'Date': start,'End Date': end,'Linked Event': event_id})
+	new_task = taskTbl.create({'Project': name,'Type': type,'Comments': comments,'Assignees': whomst_ids,'Date': start,'End Date': end,'Linked Event': event_id},typecast=True)
 	await ctx.response.send_message("Created Timeline item: [%s](%s)%s" % (new_task['fields']['Project'],new_task['fields']['Interface URL'],extra),suppress_embeds=True)
 
 @commands.has_permissions(manage_messages=True)
